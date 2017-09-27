a1b77452f1a8281ec5ee5f5b83ea03a30df52fc6
yufei_zhou <yufei.zhou@nokia-sbell.com>
2017-09-27 14:33:28 +0800

    Add deployment part
    Modify pom file because verify failed
    
    Change-Id: I4a1123bf8811853d0db1a70c61af7fdf1c476119
    Issue-ID: VFC-53
    Signed-off-by: yufei_zhou <yufei.zhou@nokia-sbell.com>

---
+++ nokia/vnfmdriver/vfcadaptorservice/vfcadaptor/src/main/java/org/onap/vfc/nfvo/driver/vnfm/svnfm/adaptor/InstantiateVnfContinueRunnable.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2016-2017, Nokia Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onap.vfc.nfvo.driver.vnfm.svnfm.adaptor;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.catalog.inf.CatalogMgmrInf;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.cbam.bo.CBAMInstantiateVnfRequest;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.cbam.bo.CBAMInstantiateVnfResponse;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.cbam.inf.CbamMgmrInf;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.constant.CommonConstants;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.constant.CommonEnum;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.constant.CommonEnum.LifecycleOperation;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.db.bean.VnfmJobExecutionInfo;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.db.repository.VnfmJobExecutionRepository;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.nslcm.bo.NslcmGrantVnfRequest;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.nslcm.bo.NslcmGrantVnfResponse;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.nslcm.bo.NslcmNotifyLCMEventsRequest;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.nslcm.bo.entity.ResourceDefinition;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.nslcm.bo.entity.VnfPackageInfo;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.nslcm.inf.NslcmMgmrInf;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.vnfmdriver.bo.InstantiateVnfRequest;
+
+
+public class InstantiateVnfContinueRunnable implements Runnable {
+	private static final Logger logger = LogManager.getLogger("InstantiateVnfContinueRunnable");
+	private CbamMgmrInf cbamMgmr;
+	private CatalogMgmrInf catalogMgmr;
+	private NslcmMgmrInf nslcmMgmr;
+	
+	private InstantiateVnfRequest driverRequest;
+	private String vnfInstanceId;
+	private String jobId;
+	
+	private VnfmJobExecutionRepository jobDbMgmr;
+	
+	private Driver2CbamRequestConverter requestConverter;
+	
+	public InstantiateVnfContinueRunnable(InstantiateVnfRequest driverRequest, String vnfInstanceId, String jobId,
+			NslcmMgmrInf nslcmMgmr, CatalogMgmrInf catalogMgmr, CbamMgmrInf cbamMgmr, Driver2CbamRequestConverter requestConverter, VnfmJobExecutionRepository dbManager)
+	{
+		this.driverRequest = driverRequest;
+		this.vnfInstanceId = vnfInstanceId;
+		this.jobId = jobId;
+		this.nslcmMgmr = nslcmMgmr; 
+		this.catalogMgmr = catalogMgmr;
+		this.cbamMgmr = cbamMgmr;
+		this.requestConverter = requestConverter;
+		this.jobDbMgmr = dbManager;
+	}
+	
+	public void run() {
+		try {
+			NslcmGrantVnfRequest grantRequest = buildNslcmGrantVnfRequest();
+			NslcmGrantVnfResponse grantResponse = nslcmMgmr.grantVnf(grantRequest);
+			handleNslcmGrantResponse(grantResponse);
+			
+			//step 2: query vnfPackage uri
+			VnfPackageInfo vnfPackageInfo = catalogMgmr.queryVnfPackage(driverRequest.getVnfPackageId());
+			
+			//step 5: instantiate vnf
+			CBAMInstantiateVnfRequest  instantiateReq = requestConverter.InstantiateReqConvert(driverRequest, grantResponse, null, null);
+			CBAMInstantiateVnfResponse cbamInstantiateResponse = cbamMgmr.instantiateVnf(instantiateReq, vnfInstanceId);
+			handleCbamInstantiateResponse(cbamInstantiateResponse, jobId);
+			
+			NslcmNotifyLCMEventsRequest nslcmNotifyReq = buildNslcmNotifyLCMEventsRequest(cbamInstantiateResponse);
+			nslcmMgmr.notifyVnf(nslcmNotifyReq, vnfInstanceId);
+			
+		} catch (ClientProtocolException e) {
+			logger.error("InstantiateVnfContinueRunnable run error ClientProtocolException", e);
+		} catch (IOException e) {
+			logger.error("InstantiateVnfContinueRunnable run error IOException", e);
+		}
+		
+	}
+	
+	private NslcmNotifyLCMEventsRequest buildNslcmNotifyLCMEventsRequest(CBAMInstantiateVnfResponse cbamInstantiateResponse) {
+		NslcmNotifyLCMEventsRequest request = new NslcmNotifyLCMEventsRequest();
+		if(CommonEnum.OperationStatus.STARTED == cbamInstantiateResponse.getStatus())
+		{
+			request.setStatus(CommonEnum.status.start);
+		}
+		else
+		{
+			request.setStatus(CommonEnum.status.result);
+			
+			//TODO the following are for the result
+//			request.setAffectedVnfc(affectedVnfc);
+//			request.setAffectedVI(affectedVI);
+//			request.setAffectedVirtualStorage(affectedVirtualStorage);
+		}
+		
+		request.setVnfInstanceId(vnfInstanceId);
+		request.setOperation(CommonConstants.NSLCM_OPERATION_INSTANTIATE);
+		request.setJobId(jobId);
+		return request;
+	}
+
+	private NslcmGrantVnfRequest buildNslcmGrantVnfRequest() {
+		NslcmGrantVnfRequest request = new NslcmGrantVnfRequest();
+		
+		request.setVnfInstanceId(vnfInstanceId);
+		request.setLifecycleOperation(LifecycleOperation.Instantiate);
+		request.setJobId(jobId);
+		
+		ResourceDefinition resource = getFreeVnfResource();
+		List<ResourceDefinition> resourceList = new ArrayList<ResourceDefinition>();
+		resourceList.add(resource);
+		request.setAddResource(resourceList);
+		
+		return request;
+	}
+
+	private ResourceDefinition getFreeVnfResource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void handleCbamInstantiateResponse(CBAMInstantiateVnfResponse cbamInstantiateResponse, String jobId) {
+		VnfmJobExecutionInfo jobInfo = jobDbMgmr.findOne(Long.getLong(jobId));
+		
+		jobInfo.setVnfmExecutionId(cbamInstantiateResponse.getId());
+		jobDbMgmr.save(jobInfo);
+	}
+
+	private void handleNslcmGrantResponse(NslcmGrantVnfResponse grantResponse) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ nokia/vnfmdriver/vfcadaptorservice/vfcadaptor/src/main/java/org/onap/vfc/nfvo/driver/vnfm/svnfm/cbam/inf/CbamMgmrInf.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2016-2017, Nokia Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onap.vfc.nfvo.driver.vnfm.svnfm.cbam.inf;
+
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.cbam.bo.CBAMCreateVnfRequest;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.cbam.bo.CBAMCreateVnfResponse;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.cbam.bo.CBAMHealVnfRequest;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.cbam.bo.CBAMHealVnfResponse;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.cbam.bo.CBAMInstantiateVnfRequest;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.cbam.bo.CBAMInstantiateVnfResponse;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.cbam.bo.CBAMQueryOperExecutionResponse;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.cbam.bo.CBAMQueryVnfResponse;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.cbam.bo.CBAMScaleVnfRequest;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.cbam.bo.CBAMScaleVnfResponse;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.cbam.bo.CBAMTerminateVnfRequest;
+import org.onap.vfc.nfvo.driver.vnfm.svnfm.cbam.bo.CBAMTerminateVnfResponse;
+
+public interface CbamMgmrInf {
+
+	public CBAMCreateVnfResponse createVnf(CBAMCreateVnfRequest cbamRequest) throws ClientProtocolException, IOException;
+
+	public CBAMInstantiateVnfResponse instantiateVnf(CBAMInstantiateVnfRequest cbamRequest, String vnfInstanceId)
+			throws ClientProtocolException, IOException;
+
+	public CBAMTerminateVnfResponse terminateVnf(CBAMTerminateVnfRequest cbamRequest, String vnfInstanceId)
+			throws ClientProtocolException, IOException;
+
+	public CBAMScaleVnfResponse scaleVnf(CBAMScaleVnfRequest cbamRequest, String vnfInstanceId)
+			throws ClientProtocolException, IOException;
+
+	public CBAMHealVnfResponse healVnf(CBAMHealVnfRequest cbamRequest, String vnfInstanceId)
+			throws ClientProtocolException, IOException;
+
+	public CBAMQueryVnfResponse queryVnf(String vnfInstanceId) throws ClientProtocolException, IOException;
+	
+	public void deleteVnf(String vnfInstanceId) throws ClientProtocolException, IOException;
+
+	public CBAMQueryOperExecutionResponse queryOperExecution(String execId) throws ClientProtocolException, IOException;
+}
