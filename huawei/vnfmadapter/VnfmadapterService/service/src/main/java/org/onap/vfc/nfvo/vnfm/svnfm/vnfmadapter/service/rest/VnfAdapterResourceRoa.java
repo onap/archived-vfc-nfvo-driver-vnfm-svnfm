5bc5a56fe65a91b2cca44f0d025a3a96298adab9
luxin <luxin7@huawei.com>
2017-09-27 17:23:25 +0800

    Remove unused parameter in restRoa
    Change-Id: Ia627e06fbe42825825a3b512b53fdff9bb32b219
    Issue-Id:VFC-425
    Signed-off-by: luxin <luxin7@huawei.com>

--- huawei/vnfmadapter/VnfmadapterService/service/src/main/java/org/onap/vfc/nfvo/vnfm/svnfm/vnfmadapter/service/rest/VnfAdapterResourceRoa.java
+++ huawei/vnfmadapter/VnfmadapterService/service/src/main/java/org/onap/vfc/nfvo/vnfm/svnfm/vnfmadapter/service/rest/VnfAdapterResourceRoa.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Huawei Technologies Co., Ltd.
+ * Copyright 2016-2017 Huawei Technologies Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,12 +16,10 @@
 
 package org.onap.vfc.nfvo.vnfm.svnfm.vnfmadapter.service.rest;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import org.onap.vfc.nfvo.vnfm.svnfm.vnfmadapter.service.adapter.impl.AdapterResourceManager;
@@ -29,7 +27,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.sf.json.JSONObject;
-
 
 /**
  * Provide interfaces of resource for VNFM.
@@ -45,7 +42,6 @@ public class VnfAdapterResourceRoa {
 
     private static final Logger LOG = LoggerFactory.getLogger(VnfAdapterResourceRoa.class);
 
-
     /**
      * Provide function of grant resource.
      * <br/>
@@ -55,13 +51,13 @@ public class VnfAdapterResourceRoa {
      * @since VFC 1.0
      */
     @GET
-    public String getAllCloudInfo(@Context HttpServletRequest context) {
+    public String getAllCloudInfo() {
         LOG.info("function=getAllCloudInfo, msg=getAllCloudInfo resource");
 
-        //Find a way to get url and pass it getAllCloud(); //NOSONAR
+        // Find a way to get url and pass it getAllCloud()
 
         AdapterResourceManager arm = new AdapterResourceManager();
-        JSONObject resultObj = arm.getAllCloud("","");
+        JSONObject resultObj = arm.getAllCloud("", "");
 
         return resultObj.getString("vim_id");
     }
