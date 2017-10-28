05e444d9445dbe21d5f427a3b5cd49edfd4eefbd
luxin <luxin7@huawei.com>
2017-10-28 14:30:00 +0800

    Fix ESR url
    Change-Id: Ic29fb67e3d3fe036aad8425ae4077e4588060371
    Issue-Id:VFC-552
    Signed-off-by: luxin <luxin7@huawei.com>

--- huawei/vnfmadapter/VnfmadapterService/service/src/main/java/org/onap/vfc/nfvo/vnfm/svnfm/vnfmadapter/service/constant/ParamConstants.java
+++ huawei/vnfmadapter/VnfmadapterService/service/src/main/java/org/onap/vfc/nfvo/vnfm/svnfm/vnfmadapter/service/constant/ParamConstants.java
@@ -110,10 +110,11 @@ public class ParamConstants {
     public static final String MSB_UNREGISTER_URL = "/api/microservices/v1/services/hw-vnfm";
 
     // /api/extsys/v1/vnfms/%s
-    public static final String ESR_GET_VNFM_URL = "/external-system/esr-vnfm-list/esr-vnfm/%s/esr-system-info-list";
+    public static final String ESR_GET_VNFM_URL =
+            "/aai/v11/external-system/esr-vnfm-list/esr-vnfm/%s/esr-system-info-list";
 
     // /api/extsys/v1/vnfms
-    public static final String ESR_GET_VNFMS_URL = "/external-system/esr-vnfm-list";
+    public static final String ESR_GET_VNFMS_URL = "/aai/v11/external-system/esr-vnfm-list";
 
     public static final String GRANT_RES_URL = "/api/resmgr/v1/resource/grant";
 
