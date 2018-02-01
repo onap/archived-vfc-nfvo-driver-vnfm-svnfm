83e5b4e36fe3e6640a4fda60b8353b8680f9af2a
Denes Nemeth <denes.nemeth@nokia.com>
2018-02-01 09:59:48 +0100

    Update Nokia driver
    Change-Id: I950afe6acbdb359cd67a448024f006a45e8fc293
    Signed-off-by: Denes Nemeth <denes.nemeth@nokia.com>
    Issue-ID: VFC-728

---
+++ nokia/vnfmdriver/deployment/src/main/resources/docker-entrypoint.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#
+# Copyright 2017, Nokia Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
+CONFIG_LOCATION=/service/application.properties
+
+function shutdown(){
+  echo "Recieved sutdown signal"
+  if [ ! -e /proc/$PID ] ; then
+    echo "The process already exited $PID"
+    exit
+  fi
+  echo "Sending shutdown signal to $PID"
+  kill -15 $PID
+  for i in `seq 0 20`; do
+    if [ ! -e /proc/$PID ] ; then
+      echo "The driver exited normally $PID"
+      exit 0
+    fi
+    sleep 10
+  done
+  echo "Terminating the driver forcefully $PID"
+  kill -9 $PID
+}
+
+function switchLine(){
+  PATTERN=$1
+  REPLACEMENT=$2
+  if [ ! -z $REPLACEMENT ]; then
+    sed -i "s/${PATTERN}.*/${PATTERN}/g" $CONFIG_LOCATION
+    sed -i "s|${PATTERN}|${PATTERN}=${REPLACEMENT}|g" $CONFIG_LOCATION
+  fi
+}
+
+function configure(){
+  if [ ! -z "$CONFIGURE" ] ; then
+     switchLine driverMsbExternalIp $EXTERNAL_IP
+     switchLine driverVnfmExternalIp $EXTERNAL_IP
+     switchLine messageBusIp $MSB_IP
+     switchLine cbamCatalogUrl $CBAM_CATALOG_URL
+     switchLine cbamLcnUrl $CBAM_LCN_URL
+     switchLine cbamKeyCloakBaseUrl $CBAM_KEYCLOAK_URL
+     switchLine cbamLcnUrl $CBAM_LCN_URL
+     switchLine cbamUsername $CBAM_USERNAME
+     switchLine cbamPassword $CBAM_PASSWORD
+     switchLine vnfmId $VNFM_ID
+     switchLine ipMap $IP_MAP
+  fi 
+}
+
+
+#during shutdown signal the ervice to stop
+trap shutdown SIGINT SIGTERM 
+
+#configure if required
+configure
+
+#start the service
+cd /service
+
+while true ; do
+  echo "Starting server"
+  java -jar driver.war --spring.config.location=file:/service/application.properties &>> /service/service.log &
+  export PID=$!
+  echo "Server process started in background with id $PID"
+  while test -e /proc/$PID; do
+    sleep 10
+  done
+  echo "Server quit (unexpected)"
+done
+
---
+++ nokia/vnfmdriver/driver/src/main/java/org/onap/vfc/nfvo/driver/vnfm/svnfm/nokia/rest/MsbApiProvider.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2016-2017, Nokia Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.vfc.nfvo.driver.vnfm.svnfm.nokia.rest;
+
+import org.onap.msb.sdk.discovery.common.RouteException;
+import org.onap.msb.sdk.discovery.entity.MicroServiceFullInfo;
+import org.onap.msb.sdk.discovery.entity.NodeInfo;
+import org.onap.msb.sdk.httpclient.msb.MSBServiceClient;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import static java.lang.Integer.valueOf;
+import static org.onap.vfc.nfvo.driver.vnfm.svnfm.nokia.util.CbamUtils.fatalFailure;
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Responsible for providing REST client to access MSB API
+ */
+@Component
+public class MsbApiProvider extends IpMappingProvider {
+    private static Logger logger = getLogger(MsbApiProvider.class);
+    @Value("${messageBusIp}")
+    private String messageBusIp;
+    @Value("${messageBusPort}")
+    private String messageBusPort;
+
+    @Autowired
+    MsbApiProvider(Environment environment) {
+        super(environment);
+    }
+
+    /**
+     * @return API to access ONAP MSB
+     */
+    public MSBServiceClient getMsbClient() {
+        return new MSBServiceClient(messageBusIp, valueOf(messageBusPort));
+    }
+
+    /**
+     * @param name    the name of the micro service
+     * @param version the version of the micro service
+     * @return the base URL of the micro service (ex. https://1.2.3.4/path )
+     */
+    public String getMicroServiceUrl(String name, String version) {
+        MicroServiceFullInfo microServiceFullInfo = getMicroServiceInfo(name, version);
+        String protocol = "http://"; //FIXME the enable_ssl field should be used, but it is not available in SDK
+        String ipAnPort = getNodeIpAnPort(microServiceFullInfo);
+        //the field name in A&AI is misleading the URL is relative path postfixed to http(s)://ip:port
+        String fullUrl = protocol + ipAnPort + microServiceFullInfo.getUrl();
+        return fullUrl;
+    }
+
+    private MicroServiceFullInfo getMicroServiceInfo(String name, String version) throws RuntimeException {
+        try {
+            return getMsbClient().queryMicroServiceInfo(name, version);
+        } catch (RouteException e) {
+            throw fatalFailure(logger, "Unable to get micro service URL for " + name + " with version " + version, e);
+        }
+    }
+
+    private String getNodeIpAnPort(MicroServiceFullInfo microServiceFullInfo) {
+        for (NodeInfo nodeInfo : microServiceFullInfo.getNodes()) {
+            if (!nodeInfo.getIp().startsWith("172.")) { // FIXME how to know which of the multiple addresses to use?
+                return mapPrivateIpToPublicIp(nodeInfo.getIp()) + ":" + nodeInfo.getPort();
+            }
+        }
+        throw fatalFailure(logger, "The " + microServiceFullInfo.getServiceName() + " service with " + microServiceFullInfo.getVersion() + " does not have any valid nodes" + microServiceFullInfo.getNodes());
+    }
+}
