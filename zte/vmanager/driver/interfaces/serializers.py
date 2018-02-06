c495a0e9025cdf6f8df411c5a03507a10449bdb8
ying.yunlong <ying.yunlong@zte.com.cn>
2018-02-06 18:21:22 +0800

    Add vfc-ztevnfmdriver serializer file
    Change-Id: I92111dbe6e31a9520e1c583f1790d920dd32f8c1
    Issue-ID: VFC-736
    Signed-off-by: ying.yunlong <ying.yunlong@zte.com.cn>

---
+++ zte/vmanager/driver/interfaces/serializers.py
@@ -0,0 +1,67 @@
+# Copyright 2018 ZTE Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from rest_framework import serializers
+
+
+class AffectedvmSerializer(serializers.Serializer):
+    extention = serializers.CharField(
+        help_text="extention",
+        max_length=255,
+        required=True,
+        allow_null=True)
+    vmid = serializers.CharField(
+        help_text="vmid",
+        max_length=255,
+        required=True,
+        allow_null=False)
+    changtype = serializers.CharField(
+        help_text="changtype",
+        max_length=255,
+        required=False,
+        allow_null=True)
+    vduid = serializers.CharField(
+        help_text="vduid",
+        max_length=255,
+        required=True,
+        allow_null=False)
+    vmname = serializers.CharField(
+        help_text="vmname",
+        max_length=255,
+        required=False,
+        allow_null=True)
+    flavour = serializers.DictField(
+        help_text="flavour",
+        child=serializers.CharField(allow_blank=True),
+        required=False,
+        allow_null=True)
+
+
+class HealReqSerializer(serializers.Serializer):
+    action = serializers.CharField(
+        help_text="action",
+        max_length=255,
+        required=True,
+        allow_null=True)
+    lifecycleoperation = serializers.CharField(
+        help_text="lifecycleoperation",
+        max_length=255,
+        required=True,
+        allow_null=False)
+    isgrace = serializers.CharField(
+        help_text="isgrace",
+        max_length=255,
+        required=False,
+        allow_null=True)
+    affectedvm = AffectedvmSerializer(help_text='affectedvm', many=True)
