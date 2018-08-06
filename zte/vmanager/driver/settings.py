cb6915484050cb9d58cddc8814115d6f15aed471
biancunkang <bian.cunkang@zte.com.cn>
2018-08-06 11:45:39 +0800

    Fix log error when run vmanager test in windows
    Change-Id: If743b404080645250537f4a08f70c70a39ca9c53
    Issue-ID: VFC-1014
    Signed-off-by: biancunkang <bian.cunkang@zte.com.cn>

--- zte/vmanager/driver/settings.py
+++ zte/vmanager/driver/settings.py
@@ -14,6 +14,7 @@
 
 import os
 import sys
+import platform
 from logging import config
 from onaplogging import monkey
 monkey.patch_all()
@@ -115,51 +116,48 @@ TIME_ZONE = 'UTC'
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-#
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': True,
-#     'formatters': {
-#         'standard': {
-#             'format': '%(asctime)s:[%(name)s]:[%(filename)s]-[%(lineno)d] [%(levelname)s]:%(message)s',
-#         },
-#     },
-#     'filters': {
-#     },
-#     'handlers': {
-#         'driver_handler': {
-#             'level': 'DEBUG',
-#             'class': 'logging.handlers.RotatingFileHandler',
-#             'filename': os.path.join(
-#                 BASE_DIR,
-#                 'logs/runtime_driver.log'),
-#             'formatter': 'standard',
-#             'maxBytes': 1024 * 1024 * 50,
-#             'backupCount': 5,
-#         },
-#     },
-#     'loggers': {
-#         'driver': {
-#             'handlers': ['driver_handler'],
-#             'level': 'DEBUG',
-#             'propagate': False},
-#     }}
 
-LOGGING_CONFIG = None
+if platform.system() == 'Windows' or 'test' in sys.argv:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': True,
+        'formatters': {
+            'standard': {
+                'format': '%(asctime)s:[%(name)s]:[%(filename)s]-[%(lineno)d] [%(levelname)s]:%(message)s',
+            },
+        },
+        'filters': {
+        },
+        'handlers': {
+            'driver_handler': {
+                'level': 'DEBUG',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename': os.path.join(
+                    BASE_DIR,
+                    'logs/runtime_driver.log'),
+                'formatter': 'standard',
+                'maxBytes': 1024 * 1024 * 50,
+                'backupCount': 5,
+            },
+        },
+        'loggers': {
+            'driver': {
+                'handlers': ['driver_handler'],
+                'level': 'DEBUG',
+                'propagate': False},
+        }}
+else:
+    LOGGING_CONFIG = None
 # yaml configuration of logging
-LOGGING_FILE = os.path.join(BASE_DIR, 'driver/log.yml')
-if 'test' in sys.argv:
-    os.system('sed -i "s|/var/log/onap/vfc/ztevnfmdriver|/tmp|" %s' % LOGGING_FILE)
-config.yamlConfig(filepath=LOGGING_FILE, watchDog=True)
+    LOGGING_FILE = os.path.join(BASE_DIR, 'driver/log.yml')
+    config.yamlConfig(filepath=LOGGING_FILE, watchDog=True)
 
 
 if 'test' in sys.argv:
-    os.system('sed -i "s|/tmp|/var/log/onap/vfc/ztevnfmdriver|" %s' % LOGGING_FILE)
 
     from driver.pub.config import config
     config.REG_TO_MSB_WHEN_START = False
     REST_FRAMEWORK = {}
-    import platform
     if platform.system() == 'Linux':
         TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
         TEST_OUTPUT_VERBOSE = True
